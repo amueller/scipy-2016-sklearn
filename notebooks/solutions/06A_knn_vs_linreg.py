d2dd526878c58b43e7e725b4c230db7bb4a009fc
Randy Olson <rso@randalolson.com>
2016-10-27 15:28:21 +0200

    Change all references from cross_validation to model_selection
    Per the scikit-learn 0.18 release

--- notebooks/solutions/06A_knn_vs_linreg.py
+++ notebooks/solutions/06A_knn_vs_linreg.py
@@ -1,5 +1,5 @@
 from sklearn.datasets import load_boston
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 
