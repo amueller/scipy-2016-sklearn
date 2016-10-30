0548865178e5140698283b257af11f628dadfaeb
Randy Olson <rso@randalolson.com>
2016-10-30 17:56:23 +0100

    Typo fix in notebook 6 solution

--- notebooks/solutions/06A_knn_vs_linreg.py
+++ notebooks/solutions/06A_knn_vs_linreg.py
@@ -21,6 +21,6 @@ print('Linear Regression Train/Test: %.3f/%.3f' %
        linreg.score(X_test, y_test)))
 
 knnreg.fit(X_train, y_train)
-print('Linear Regression Train/Test: %.3f/%.3f' %
+print('KNeighborsRegressor Train/Test: %.3f/%.3f' %
       (knnreg.score(X_train, y_train),
        knnreg.score(X_test, y_test)))
